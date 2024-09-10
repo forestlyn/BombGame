b9fdb2d384c454108c1a85885c8d0cf7a5d74392
forestlyn <yang2797817213@qq.com>
2024-09-10 16:05:37 +0800

    删除错误引用导致无法打包

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,10 +1,7 @@
 using MyInputSystem;
-using MyTools.MyCoroutines;
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.PlayerSettings;
 
 public class Player : MapObject
 {
